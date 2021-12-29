5c9fa42ef90100601df9b5f84cce0f8c78ac7546
Noman5237 <anonyman637@gmail.com>
2021-12-30 03:16:43 +0600

    handled increment and decrement counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,18 +8,40 @@ class Counter extends Component {
 		};
 	}
 
+	decrementCounter = () => {
+		const { value } = this.state;
+		this.setState({
+			value: value - 1,
+		});
+	};
+
+	incrementCounter = () => {
+		const { value } = this.state;
+		this.setState({
+			value: value + 1,
+		});
+	};
+
 	render() {
 		const { value } = this.state;
 
 		return (
 			<div className="grid grid-cols-3 bg-gray-600 w-48 rounded-full py-2 m-2">
-				<p className="m-auto bg-red-700 rounded-full w-8 text-center text-white">
+				<button
+					type="button"
+					onClick={this.incrementCounter}
+					className="m-auto bg-red-700 rounded-full w-8 text-center text-white"
+				>
 					+
-				</p>
+				</button>
 				<p className="m-auto text-white">{value}</p>
-				<p className="m-auto bg-yellow-500 rounded-full w-8 text-center text-white">
+				<button
+					type="button"
+					onClick={this.decrementCounter}
+					className="m-auto bg-yellow-500 rounded-full w-8 text-center text-white"
+				>
 					-
-				</p>
+				</button>
 			</div>
 		);
 	}
